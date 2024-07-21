d644e14bc8e289cec2682e49cb02013ccd5ecf00
chetanpatil0304 <patilchetan0304@gmail.com>
2024-07-21 20:01:29 +0530

    new

---
+++ C/Assignment/Assignment 7/1_10.c
@@ -0,0 +1,15 @@
+ int n1;
+    printf("enter size for array 1: ");
+    scanf("%d",&n1);
+
+    int n2;
+    printf("enter size for array 2: ");
+    scanf("%d",&n2);
+
+    int arr1[n1];
+    
+    printf("enter first array: ");
+    for (int i = 0; i < n1; i++)
+    {
+        scanf("%d",&arr1[i]);
+    }
---
+++ C/Classwork/String/strupr.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+void main (){
+
+    char str [40]= "Heya Welcome";
+
+    strupr(str);
+
+    puts(str);
+
+
+
+    //char *strupr(char *str);
+}
